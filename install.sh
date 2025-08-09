a519618e278b167a47ffaa9f5ecf2cd2b2214597
Stefano Francavilla <stefanofrancavilla@MACBOOKPRO-03EE.station>
2025-08-09 23:07:06 +0200

    update dotfiles directory

--- install.sh
+++ install.sh
@@ -8,13 +8,10 @@ set -eo pipefail
 
 echo "--- Starting Archade Setup ---"
 
-# Find the directory where the script is located
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 # Run the setup scripts in order
-bash "${SCRIPT_DIR}/scripts/01-desktop.sh"
-bash "${SCRIPT_DIR}/scripts/02-apps.sh"
-bash "${SCRIPT_DIR}/scripts/03-dotfiles.sh"
+source ./scripts/01-desktop.sh
+source ./scripts/02-apps.sh
+source ./scripts/03-dotfiles.sh
 
 echo "#####################################################"
 echo "#                                                   #"
--- scripts/03-dotfiles.sh
+++ scripts/03-dotfiles.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-# Stop on any error
 set -eo pipefail
 
-# This line ensures stow is installed before we try to use it.
 echo "--- Installing stow for dotfile management ---"
 sudo pacman -S --noconfirm --needed stow
 
 echo "--- Stowing dotfiles ---"
 
-# This script should be run from the root of the Archade directory.
 DOTFILES_DIR="dotfiles"
 
-# List of packages/configs to stow.
-PACKAGES=(
-    hyprland
-    waybar
-    kitty
+# List of directories to stow
+DIRECTORIES=(
+    "hyprland"
+    "kitty"
+    "zsh"
+    "waybar"
 )
 
-# Stow each package.
-for package in ${PACKAGES[@]}; do
-    echo "Stowing ${package}..."
-    stow -D ${package} -t ~ --dir=${DOTFILES_DIR}
-    stow -S ${package} -t ~ --dir=${DOTFILES_DIR}
+# Stow the directories
+for dir in "${DIRECTORIES[@]}"; do
+    echo "--- Stowing $dir ---"
+    stow -D ${dir} -t ~ --dir=${DOTFILES_DIR}
+    stow -S ${dir} -t ~ --dir=${DOTFILES_DIR}
 done
 
-echo "--- Dotfiles have been stowed successfully! ---"
+echo "--- Dotfile management complete ---"
